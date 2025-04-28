6a175f43419b4b4f408be72a9cad8ba4740fe83d
E.T. Lee <gomletghost@gmail.com>
2025-04-27 21:37:14 -0700

    ported main js and two empty scenes

---
+++ runningbeast/main.js
@@ -0,0 +1,24 @@
+import Phaser from "phaser";
+import Game from "./scenes/game"
+import GameOver from "./scenes/gameover"
+
+const config = {
+    width: 600,
+    height: 300,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    autoRound: false,
+    parent: "game-container",
+    physics: {
+        default: "arcade",
+        arcade {
+            gravity: { y: 350 }
+            debug: true,
+        },
+    },
+    scene: [Game, GameOver],
+}
+
+const game = new Phaser.Game(config)
